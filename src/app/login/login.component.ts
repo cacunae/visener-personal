c9889bb7d36f75c0b9caef7a18189621c1f27533
junotjeanlouis <junotjeanlouis@4labss.cl>
2021-07-27 18:53:42 -0400

    Solucion a incidencias con login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,13 +8,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-}) 
+})
 export class LoginComponent implements OnInit {
   email: string;
   password: string;
-  error:boolean = false;
+  error: boolean = false;
 
-  constructor(private dataService : DataService, private router : Router, public snackBar: MatSnackBar) {
+  constructor(private dataService: DataService, private router: Router, public snackBar: MatSnackBar) {
     this.dataService.user = null;
     localStorage.clear();
   }
@@ -25,36 +25,41 @@ export class LoginComponent implements OnInit {
   login() {
   }
 
-  validaUsuario(){
+  validaUsuario() {
     this.error = false;
-    this.dataService.login(this.email, Md5.hashStr(this.password)).then((result:any) => {
-      if(result.rows && result.rows[0] && result.rows[0].id){
-        this.dataService.setUser(result.rows[0].value);
-        if(this.dataService.user.entity === "professional"){
-          this.dataService.getData("/" + result.rows[0].value.role).then((features: any) => {
-            let tmpFeatures:any[] = [];
-            for (let feature of features.features) {
-              let index = features.features.findIndex((item: any) => item.name === feature.name);
-              if (index>-1 && feature.actions && feature.actions.length>0) {
-                tmpFeatures.push(feature);
-              };
-            }
-            if (tmpFeatures.length > 0) {
-              result.rows[0].value.features = tmpFeatures;
-              this.dataService.setUser(result.rows[0].value);
-            }
-            this.router.navigateByUrl("/professional");
-          });
-        }else if(this.dataService.user.entity === "patient"){
-          this.router.navigateByUrl("/patient");
-        }else{
-          this.snackBar.open('Hubo un problema al cargar su perfil.', 'ERR', {duration: 5000});
-          this.router.navigateByUrl("/login");
+    if (/[!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/.test(this.email)) {
+      this.error = true;
+      this.snackBar.open('Usuario o contraseña errónea.', 'OK', { duration: 5000 });
+    } else {
+      this.dataService.login(this.email, Md5.hashStr(this.password)).then((result: any) => {
+        if (result.rows && result.rows[0] && result.rows[0].id) {
+          this.dataService.setUser(result.rows[0].value);
+          if (this.dataService.user.entity === "professional") {
+            this.dataService.getData("/" + result.rows[0].value.role).then((features: any) => {
+              let tmpFeatures: any[] = [];
+              for (let feature of features.features) {
+                let index = features.features.findIndex((item: any) => item.name === feature.name);
+                if (index > -1 && feature.actions && feature.actions.length > 0) {
+                  tmpFeatures.push(feature);
+                };
+              }
+              if (tmpFeatures.length > 0) {
+                result.rows[0].value.features = tmpFeatures;
+                this.dataService.setUser(result.rows[0].value);
+              }
+              this.router.navigateByUrl("/professional");
+            });
+          } else if (this.dataService.user.entity === "patient") {
+            this.router.navigateByUrl("/patient");
+          } else {
+            this.snackBar.open('Hubo un problema al cargar su perfil.', 'OK', { duration: 5000 });
+            this.router.navigateByUrl("/login");
+          }
+        } else {
+          this.error = true;
+          this.snackBar.open('Usuario o contraseña errónea.', 'OK', { duration: 5000 });
         }
-      }else{
-        this.error = true;
-        this.snackBar.open('Usuario o contrasela errónea.', 'ERR', {duration: 5000});
-      }
-    });
+      });
+    }
   }
 }
